6bfa1069faa7000f22f6c4687949d9ee30a1939e
PureCrouch <105855919+PureCrouch@users.noreply.github.com>
2022-05-19 21:21:45 +0300

    Update solution3.py

--- homework4/solution3.py
+++ homework4/solution3.py
@@ -1,9 +1,8 @@
-#хэш-таблица o(n)
-
-def twoSum(self, nums, target):
-    a = {}
-    for i, value in enumerate(nums):         #используем enumerate для получения индекса элемента и его значения
-        ost = target - nums[i]
-        if ost in a:
-            return [i, a[ost]]
-        a[value] = i 
+class Solution(object):
+    def twoSum(self, nums, target):
+        a = {}
+        for i, value in enumerate(nums):         #используем enumerate для получения индекса элемента и его значения
+            ost = target - nums[i]
+            if ost in a:
+                return [i, a[ost]]
+            a[value] = i 
