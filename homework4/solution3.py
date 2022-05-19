e80c0e13b05b751d8f9309238fe52ab26d2fa99a
PureCrouch <105855919+PureCrouch@users.noreply.github.com>
2022-05-19 21:19:23 +0300

    Update solution3.py

--- homework4/solution3.py
+++ homework4/solution3.py
@@ -3,7 +3,7 @@
 def twoSum(self, nums, target):
     a = {}
     for i, value in enumerate(nums):         #используем enumerate для получения индекса элемента и его значения
-        remaining = target - nums[i]
-        if remaining in a:
-            return [i, a[remaining]]
+        ost = target - nums[i]
+        if ost in a:
+            return [i, a[ost]]
         a[value] = i 
