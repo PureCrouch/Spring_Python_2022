4d11fb7256e882ffc3102ee3000e6d867df359dd
PureCrouch <105855919+PureCrouch@users.noreply.github.com>
2022-05-19 21:00:51 +0300

    Update solution3.py

--- homework4/solution3.py
+++ homework4/solution3.py
@@ -1,9 +1,9 @@
 #хэш-таблица o(n)
 
 def twoSum(self, nums, target):
-    seen = {}
+    a = {}
     for i, value in enumerate(nums):         #используем enumerate для получения индекса элемента и его значения
         remaining = target - nums[i]
-        if remaining in seen:
-            return [i, seen[remaining]]
-        seen[value] = i 
+        if remaining in a:
+            return [i, a[remaining]]
+        a[value] = i 
